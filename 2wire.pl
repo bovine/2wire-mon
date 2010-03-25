9ae44b41e012ea0f3e985cc18d389b22100f481f
jlawson <jlawson@dexter.bovine.net>
2010-03-25 00:58:18 -0500

    update to support version 6.1.9.24-enh.tm of 2Wire sofware

--- 2wire.pl
+++ 2wire.pl
@@ -4,56 +4,107 @@ use strict;
 use LWP::UserAgent;
 use RRDs;
 
+use constant HOMEPORTAL_VERSION => 6;
 use constant HOMEPORTAL_URL => 'http://homeportal.gateway.2wire.net/xslt';
-use constant ADMIN_PASSWORD => 'zse4rfv123';
+use constant ADMIN_PASSWORD => 'your-router-password';        # only needed for version 5
 use constant RRD_FILENAME => "monitor.rrd";
+use constant DEBUG => 0;
 
 
 my $ua = LWP::UserAgent->new;
 $ua->timeout(10);
 push @{ $ua->requests_redirectable }, 'POST';
 
-
-my %params = ( 'PAGE' => 'A02_POST',
-               'NEXTPAGE' => 'J03',
-               'PASSWORD' => ADMIN_PASSWORD
-    );
-my $response = $ua->post(HOMEPORTAL_URL, \%params);
-
 my %stats;
-if ($response->is_success) {
-    my $html = $response->content;
-    die if $html !~ m|<title>2WIRE MDC - Broadband Link Statistics</title>|;
-    $html =~ s/^.*?<div class="mdcpagetitlebar">//s
-        or die "failed to find mdcpagetitlebar";
-    $html =~ s/^.*?<td colspan="10" class="mdcdotted">//s
-        or die "failed to find mdcdotted";
-    my @headings;
-    while ($html =~ m|<tr>(.*?)</tr>|gs) {
-        my $htmlrow = $1;
-        my @columns;
-        while ($htmlrow =~ m|<td.*?>(.*?)</td>|gs) {
-            push(@columns, $1);
-        };
-        if (!scalar(@headings)) {
-            @headings = @columns;
-        } else {
-            my $name = $columns[0];
-            for (my $i = 1; $i < scalar(@columns); $i++) {
-                next if !length($columns[$i]);
-                $stats{$name}{$headings[$i]} = $columns[$i];
-            }
-        }
+
+sub fetch_version5_stats () {
+    # Tested with Software Version 5.29.105.94
+    my %params = ( 'PAGE' => 'A02_POST',
+		   'NEXTPAGE' => 'J03',
+		   'PASSWORD' => ADMIN_PASSWORD
+	);
+    my $response = $ua->post(HOMEPORTAL_URL, \%params);
+
+    if ($response->is_success) {
+	my $html = $response->content;
+	
+	die "Could not find expected HTML" 
+	    if $html !~ m|<title>2WIRE MDC - Broadband Link Statistics</title>|;
+	$html =~ s/^.*?<div class="mdcpagetitlebar">//s
+	    or die "failed to find mdcpagetitlebar";
+	$html =~ s/^.*?<td colspan="10" class="mdcdotted">//s
+	    or die "failed to find mdcdotted";
+	my @headings;
+	while ($html =~ m|<tr>(.*?)</tr>|gs) {
+	    my $htmlrow = $1;
+	    my @columns;
+	    while ($htmlrow =~ m|<td.*?>(.*?)</td>|gs) {
+		push(@columns, $1);
+	    };
+	    if (!scalar(@headings)) {
+		@headings = @columns;
+	    } else {
+		my $name = $columns[0];
+		$name =~ s/:$//;   # strip trailing colon
+		for (my $i = 1; $i < scalar(@columns); $i++) {
+		    next if !length($columns[$i]);
+		    $stats{$name}{$headings[$i]} = $columns[$i];
+		}
+	    }
+	}
+    }
+    else {
+	die $response->status_line;
     }
 }
-else {
-    die $response->status_line;
+
+sub fetch_version6_stats() {
+    # tested with Software Version 6.1.9.24-enh.tm
+    my $response = $ua->get(HOMEPORTAL_URL . "?PAGE=C_1_0");
+
+    if ($response->is_success) {
+	my $html = $response->content;
+	die "Could not find expected HTML" 
+	    if $html !~ m|<h2>Traffic Statistics</h2>.*?<table.*?>(.*?)</table>|s;
+	$html = $1;
+
+	my @headings;
+	while ($html =~ m|<tr>(.*?)</tr>|gs) {
+	    my $htmlrow = $1;
+	    my @columns;
+	    while ($htmlrow =~ m|<t[dh].*?>(.*?)</t[dh]>|gs) {
+		push(@columns, $1);
+	    };
+	    if (!scalar(@headings)) {
+		@headings = @columns;
+	    } else {
+		my $name = $columns[0];
+		for (my $i = 1; $i < scalar(@columns); $i++) {
+		    next if !length($columns[$i]);
+		    $stats{$name}{$headings[$i]} = $columns[$i];
+		}
+	    }
+	}
+    }
+    else {
+	die $response->status_line;
+    }
 }
 
-#print Dumper(\%stats);
-#print "Transmit: " . $stats{'Transmit:'}{'Bytes'} . "\n";
-#print "Receive: " . $stats{'Receive:'}{'Bytes'} . "\n";
 
+if (HOMEPORTAL_VERSION == 5) {
+    fetch_version5_stats();
+} elsif (HOMEPORTAL_VERSION == 6) {
+    fetch_version6_stats();
+} else {
+    die "Unsupported version @{[ HOMEPORTAL_VERSION ]} specified."
+}
+if (DEBUG) {
+    use Data::Dumper;
+    print Dumper(\%stats);
+    print "Transmit: " . $stats{'Transmit'}{'Bytes'} . "\n";
+    print "Receive: " . $stats{'Receive'}{'Bytes'} . "\n";
+}
 
 # If the RRD does not already exist, create it.
 if (! -f RRD_FILENAME) {
@@ -69,8 +120,8 @@ if (! -f RRD_FILENAME) {
 
 # Save to RRD database.
 RRDs::update (RRD_FILENAME, "--template", "input:output",
-              "N:" . $stats{'Receive:'}{'Bytes'} .
-              ':' . $stats{'Transmit:'}{'Bytes'})
+              "N:" . $stats{'Receive'}{'Bytes'} .
+              ':' . $stats{'Transmit'}{'Bytes'})
     or warn "Failed to update RRD";
 
 exit 0;
